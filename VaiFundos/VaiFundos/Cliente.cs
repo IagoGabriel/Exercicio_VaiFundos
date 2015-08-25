fa90f75ef077a74f8367ab7bc78da9f48cfa89b9
IagoGabriel <iagogabriel@ucl.br>
2015-08-25 17:24:23 -0300

    Criando a classe Cliente

---
+++ VaiFundos/VaiFundos/Cliente.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class Cliente
+    {
+        private int codCliente;
+        private String nome;
+        private String endereco;
+        private String cpf;
+        private String telefone;
+        private DateTime datacadastro;
+
+
+
+        public Cliente(int codCliente, string nome, string endereco, string cpf, string telefone, DateTime datacadastro) {
+
+            this.codCliente = codCliente;
+            this.nome = nome;
+            this.endereco = endereco;
+            this.cpf = cpf;
+            this.telefone = telefone;
+            this.datacadastro = datacadastro;
+        
+        }
+
+
+        public void setCodC1iente(int codCliente){
+        this.codCliente = codCliente;
+        }
+
+        public int getCodCliente() {
+            return codCliente;
+        }
+
+
+        public void setCodCliente(string nome){
+            this.nome = nome;
+        }
+
+        public string getNome() {
+            return nome;
+        }
+
+        public void setEndereco(string endereco) {
+            this.endereco = endereco;
+        }
+
+        public string getEndereco(){
+        return endereco;
+        } 
+        
+        public void setCpf(string cpf) {
+            this.cpf = cpf;
+        }
+
+        public string getCpf(){
+        return cpf;
+        } 
+
+        public void setTelefone(string telefone) {
+            this.telefone = telefone;
+        }
+
+        public string getTelefone(){
+        return telefone;
+        }
+
+        public void setDatacadastro(DateTime datacadastro)
+        {
+            this.datacadastro = datacadastro;
+        }
+
+        public DateTime getdatacadastro(){
+            return datacadastro;
+        }
+    }
+
+
+}
