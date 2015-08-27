15ccb85d9ee1559c1eb7c9e2c133e90889795f30
marianemonteiro <marianemq@ucl.br>
2015-08-27 11:06:45 -0300

    Dolar recebe atributos da Classe Moeda

---
+++ VaiFundos/VaiFundos/Dolar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class Dolar : Moeda
+    {
+        private string cod_iso;
+        private string titulo_moeda;
+
+
+        public Dolar(string cod_iso, string titulo_moeda)
+        {
+
+            this.cod_iso = cod_iso;
+            this.titulo_moeda = titulo_moeda;
+
+
+        }
+
+        public string getCod_iso()
+        {
+            return cod_iso;
+        }
+
+        public void setCod_iso(string cod_iso)
+        {
+            this.cod_iso = cod_iso;
+        }
+
+        public string getTitulo_moeda()
+        {
+            return titulo_moeda;
+        }
+
+        public void setTituloMoeda(string titulo_moeda)
+        {
+            this.titulo_moeda = titulo_moeda;
+        }
+    }
+}
